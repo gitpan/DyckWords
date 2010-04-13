61096d86d7b960a9c1c617d7a8eb4dc64fc61c89
Mark Mertel <mmertel@cpan.org>
2010-04-13 05:50:55 +0000

    Import of MMERTEL/DyckWords-0.02 from CPAN.
    gitpan-cpan-distribution: DyckWords
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         MMERTEL/DyckWords-0.02.tar.gz
    gitpan-cpan-author:       MMERTEL
    gitpan-cpan-maturity:     released

--- lib/Math/DyckWords.pm
+++ lib/Math/DyckWords.pm
@@ -3,10 +3,8 @@
 
 =head1 NAME
 
-Math::DyckWords - Perl module for generating Dyck words. The functions
-in this moduloe are based on those presented in the scholarly paper
-"Generating and ranking of Dyck words" by Zoltan Kasa available on-line
-at http://arxiv4.library.cornell.edu/pdf/1002.2625
+Math::DyckWords - Perl module for generating Dyck words. Dyck words
+are named after the mathematician Walther von Dyck.
 
 =head1 SYNOPSIS
 
@@ -14,14 +12,57 @@ at http://arxiv4.library.cornell.edu/pdf/1002.2625
                           dyck_words_by_position
                           dyck_words_by_swap
                           ranking
-                          unranking );
+                          unranking
+                          catalan_number );
 
-  foreach my $word ( dyck_words_by_lex( 4 ) ) {
-      print "word: $word\n";
-      print "rank:" . ranking( $word ) . "\n";
-  }
+  @words = dyck_words_by_lex( 4 );
+  @words = dyck_words_by_position( 4 );
+  @words = dyck_words_by_swap( 4 );
+  $rank  = ranking( '01010101' );
+  $word  = unranking( 3, 2 );
 
 =head1 DESCRIPTION
+
+Dyck words are even numbered string of X's and Y's, or 0's and 1's,
+or any other binary alphabet for that matter, such that no initial
+segment has more Y's or 1's.  The following are the Dyck words of
+length 2n where n = 3:
+    000111
+    010011
+    010101
+    001101
+    001011
+
+Another common use of Dyck words is in dealing with the balanced
+parenthesis problem. Substituting the left and right parentheses
+for the 0's an 1's listed above we have:
+    ((()))
+    ()(())
+    ()()()
+    (())()
+    (()())
+
+There is also a relationship between Dyck words and Catalan numbers.
+Catalan numbers have many applications in combinatorics and consists
+of a sequence of ever increasing integers following the formula:
+
+    (2n)!/(n!(n+1)!)
+
+The first few numbers in the Catalan sequence are:
+
+    1, 1, 2, 5, 14, 132, 429, 1430, 4862, 16796, 58786, 208012
+
+The relationship between Dyck words and the Catalan sequence can
+be easily seen as the nth Catalan number is equal to the number of
+permutations, or unique Dyck words of length 2n. For example,
+the 3rd Catalan number, using a zero index, is 5. This is the same
+number of Dyck words of length 2n where n = 3.
+
+The algorithms in this module are based on those presented in the
+scholarly paper "Generating and ranking of Dyck words" by Zoltan Kasa
+available on-line at http://arxiv4.library.cornell.edu/pdf/1002.2625,
+and the provide three different Dyck word generators - lexigraphical,
+positional, and one that generates Dyck words by swapping characters.
 
 =head2 EXPORT
 
@@ -37,6 +78,7 @@ use warnings;
 
 use Carp;
 use Data::Dumper;
+use Math::BigInt;
 use Exporter;
 
 our $VERSION = '0.01';
@@ -45,13 +87,19 @@ our @EXPORT_OK = qw( dyck_words_by_lex
                      dyck_words_by_position
                      dyck_words_by_swap
                      ranking
-                     unranking );
+                     unranking
+                     catalan_number );
 
-# The algorithm that generates all Dyck words in lexicographic
-# order is obvious. Let us begin with 0 in the 1st position,
-# and add 0 or 1 each time the Dyck-property remains valid.
-# In the following algorithm 2n is the length of a Dyck word,
-# n0 counts the 0s, and n1 the 1s.
+=over
+
+=item dyck_words_by_lex( $n )
+
+This algorithm returns a list of all Dyck words of length 2n in ascending
+lexicographic order, i.e. 000111, 001011, 001101, 010011, 010101
+
+=back
+
+=cut
 
 my @words;
 
@@ -92,6 +140,16 @@ sub dyck_words_by_lex {
     return @words;
 }
 
+=over
+
+=item dyck_words_by_position( $n )
+
+This algorithm returns a list of all Dyck words of length 2n in descending
+lexicographic order, i.e. 010101, 010011, 001101, 001011, 000111.
+
+=back
+
+=cut
 
 sub dyck_words_by_position {
     my $n = shift;
@@ -132,8 +190,18 @@ sub dyck_words_by_position {
     return @words;
 }
 
-# translates an array of integer values which indicate
-# the position of 1's in the resultant Dyck word
+=over
+
+=item translate_positions( @p )
+
+This function translates an array of integer values indicating
+the position of 1's in the resultant Dyck word, and is called by
+the dyck_words_by_position function.
+
+=back
+
+=cut
+
 sub translate_positions( @ ) {
     my $n = scalar @_;
 
@@ -148,9 +216,18 @@ sub translate_positions( @ ) {
     return $word;
 }
 
-# Generating by changing 10 in 01
-# The basic idea is to change the first occurence of 10 in 01
-# to get a new Dyck word. We begin with 0101 ...  01.
+=over
+
+=item dyck_words_by_position( $n )
+
+This algorithm returns a list of all Dyck words of length 2n in no
+particular order, i.e. 010101, 001101, 001011, 000111, 010011. This
+is done by changing the first occurrence of '10' to '01'.
+
+=back
+
+=cut
+
 sub dyck_words_by_swap {
     my ( $n, $X, $k ) = @_;
 
@@ -180,13 +257,20 @@ sub dyck_words_by_swap {
     }
 }
 
-# Ranking Dyck words means to determine the position of a Dyck
-# word in a given ordered sequence of all Dyck words.
-# Algorithm dyck_words_by_position generates all Dyck word in
-# reverse lexicographic order. For ranking these words we will
-# use the following function , where f(n,i,j) represents the number
-# of paths between (0,0) and (i,j) not crossing the diagonal
-# x = y of the grid.
+=over
+
+=item monotonic_path_count( $n, $i, $j )
+
+Ranking Dyck words means to determine the position of a Dyck
+word in a given ordered sequence of all Dyck words.
+For ranking these words we will use the following function,
+where f(n,i,j) represents the number of paths between (0,0)
+and (i,j) not crossing the diagonal x = y of the grid.
+
+=back
+
+=cut
+
 sub monotonic_path_count {
     my ( $n, $i, $j ) = @_;
 
@@ -205,8 +289,17 @@ sub monotonic_path_count {
     }
 }
 
-# converts a dyck word string of 1's and 0's into a list
-# of positions where the 1's are located, i.e. 2468
+=over
+
+=item positions( $w )
+
+This function converts a Dyck word string of 1's and 0's into a list
+of positions where the 1's are located, i.e. 2468 => 01010101
+
+=back
+
+=cut
+
 sub positions( $ ) {
     my $w = shift;
 
@@ -219,6 +312,17 @@ sub positions( $ ) {
     }
     return @p;
 }
+
+=over
+
+=item ranking( $w )
+
+This function returns the rank of an individual Dyck word $w in the
+list of all Dyck words of the same length.
+
+=back
+
+=cut
 
 sub ranking( $ ) {
     my @b = positions( shift );
@@ -238,6 +342,16 @@ sub ranking( $ ) {
     }
     return $nr;
 }
+
+=over
+
+=item unranking( $n, $r )
+
+This function returns the rank $r Dyck word of length $n.
+
+=back
+
+=cut
 
 sub unranking( $$ ) {
     my ( $n, $nr ) = @_;
@@ -266,6 +380,29 @@ sub unranking( $$ ) {
     shift @b;
 
     return translate_positions( @b );
+}
+
+=over
+
+=item catalan_number( $n )
+
+Using the formula - (2n)!/(n!(n+1)!) - this function returns the
+corresponding number $n from the Catalan sequence.
+
+=back
+
+=cut
+
+sub catalan_number( $ ) {
+    my $x = shift;
+
+    my $X = Math::BigInt->new( $x );
+    my $Y = $X->copy;
+    my $Z = $X->copy;
+
+    return $X->bmul( 2 )->bfac->bdiv(
+        $Y->bfac->bmul( $Z->badd( 1 )->bfac )
+    );
 }
 
 1;
--- t/Math-DyckWords.t
+++ t/Math-DyckWords.t
@@ -1,4 +1,4 @@
-use Test::More tests => 6;
+use Test::More tests => 7;
 BEGIN { use_ok( 'Math::DyckWords' ) };
 
 my @words = Math::DyckWords::dyck_words_by_lex( 5 );
@@ -18,3 +18,5 @@ ok( $rank == 19, "ranking" );
 my $word = Math::DyckWords::unranking( 5, 19 );
 ok( $word eq '0011000111', "unranking" );
 
+my $catalan_number = Math::DyckWords::catalan_number( 300 );
+ok( $catalan_number eq '448863594671741755862042783981742625904431712455792292112842929523169934910317996551330498997589600726489482164006103817421596314821101633539230654646302151568026806610883615856' );
